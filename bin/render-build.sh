9ec5096840a1feae7a2ea03a447c515597f40969
robert <ralabi408@gmail.com>
2025-08-19 20:34:41 +0000

    fix: make bin/rails executable for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+
+# Run migrations and seed database (if needed)
+bundle exec rails db:migrate
+# bundle exec rails db:seed
